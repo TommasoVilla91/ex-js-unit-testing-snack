40f7fd9cd986eb6784c4338612f2c4f31d899274
TommasoVilla91 <tommasovilla@yahoo.it>
2025-04-23 13:05:37 +0200

    feat: snack 2, 3, 4, 5

--- test-vari.js
+++ test-vari.js
@@ -1,5 +1,33 @@
+function getInitials(string){
+    const dividedName = string.split(" ");
+    const firstLetters = dividedName.map(n => [...n][0] += ".");
+    return firstLetters.join("");
+};
 
+function createSlug(string){
+    const divideString = string.split(" ");
+    if(divideString.length > 1){
+        return divideString.toString().replace(",", "-");
+    } else {
+        return divideString[0].toLowerCase();
+    }
+};
+
+function average(array){
+    const sum = array.reduce((acc, n) => {
+        return acc + n;
+    }, 0);
+    return sum / array.length;
+};
+
+function isPalindrome(string){
+    const reverseString = ([...string].reverse()).join("").toLowerCase();
+    return reverseString === string.toLowerCase();
+};
 
 module.exports = {
-    
+    getInitials,
+    createSlug,
+    average,
+    isPalindrome,
 }
--- test-vari.test.js
+++ test-vari.test.js
@@ -1,13 +1,32 @@
-const {  } = require("./test-vari.js");
-
-function getInitials(string){
-    const dividedName = string.split(" ");
-    const firstLetters = dividedName.map(n => [...n][0] += ".");
-    return firstLetters.join("");
-};
+const { getInitials, createSlug, average, isPalindrome } = require("./test-vari.js");
 
 // Snack 1
 test("La funzione getInitials restituisce le iniziali di un nome completo", () => {
     const initials = getInitials("Livio Lenta");
     expect(initials).toBe("L.L.");
 });
+
+
+// Snack 2
+test("La funzione createSlug restituisce una stringa in lowercase", () => {
+    expect(createSlug("Polenta")).toBe("polenta");
+});
+
+
+// Snack 3
+test("La funzione average calcola la media aritmetica di un array di numeri", () => {
+    expect(average([1, 7, 33, 82])).toBe(30.75);
+});
+
+
+// Snack 4
+test("La funzione createSlug sostituisce gli spazi con -", () => {
+    expect(createSlug("Polenta taragna")).toBe("Polenta-taragna");
+});
+
+
+// Snack 5
+test("La funzione isPalindrome verifica se una stringa è un palindromo", () => {
+    expect(isPalindrome("Anna")).toBeTruthy();
+    expect(isPalindrome("Pollo")).toBeFalsy();
+})
